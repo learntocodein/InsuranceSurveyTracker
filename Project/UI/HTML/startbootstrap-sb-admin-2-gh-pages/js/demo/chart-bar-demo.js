1b7a017d4ae75a0e1a873a4321a9bd4672130325
FaizlM <42780731+FaizlM@users.noreply.github.com>
2019-12-27 17:16:07 +0530

    code refactoring

--- Project/UI/HTML/startbootstrap-sb-admin-2-gh-pages/js/demo/chart-bar-demo.js
+++ Project/UI/HTML/startbootstrap-sb-admin-2-gh-pages/js/demo/chart-bar-demo.js
@@ -32,13 +32,13 @@ var ctx = document.getElementById("myBarChart");
 var myBarChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["National", "New India", "Oriental", "United India", "Private"],
     datasets: [{
       label: "Revenue",
       backgroundColor: "#4e73df",
       hoverBackgroundColor: "#2e59d9",
       borderColor: "#4e73df",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [210000, 175000, 100000, 225000, 50000, 75000],
     }],
   },
   options: {
@@ -68,12 +68,12 @@ var myBarChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 300000,
           maxTicksLimit: 5,
           padding: 10,
           // Include a dollar sign in the ticks
           callback: function(value, index, values) {
-            return '$' + number_format(value);
+            return 'Rs ' + number_format(value);
           }
         },
         gridLines: {
@@ -103,7 +103,7 @@ var myBarChart = new Chart(ctx, {
       callbacks: {
         label: function(tooltipItem, chart) {
           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+          return datasetLabel + ': Rs ' + number_format(tooltipItem.yLabel);
         }
       }
     },
